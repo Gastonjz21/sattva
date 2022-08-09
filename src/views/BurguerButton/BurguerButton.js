238cf38a4abd0c0c55c24cba9c20138f41a420fd
Gastonjz21 <gastonjzabala@gmail.com>
2022-08-09 06:19:01 -0300

    Agregado de Navegacion Responsive con errores

--- src/views/BurguerButton/BurguerButton.js
+++ src/views/BurguerButton/BurguerButton.js
@@ -2,13 +2,7 @@ import './burguerbuttom.css'
 
 function BurguerButton(props) {
   return (
-      <div onClick={props.handleClick} className={`icon nav-icon-1 ${props.clicked ? 'open' : ''}`}>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
+      <div onClick={props.handleClick} className={`icon nav-icon-5 ${props.clicked ? 'open' : ''}`}>
         <span></span>
         <span></span>
         <span></span>
